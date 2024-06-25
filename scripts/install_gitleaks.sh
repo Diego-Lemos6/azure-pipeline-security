60dcf85379d4f7cbd0b7154c7d407348acb5070e
Diego-Lemos6 <71842566+Diego-Lemos6@users.noreply.github.com>
2024-06-25 13:26:24 -0300

    Update install_gitleaks.sh

--- scripts/install_gitleaks.sh
+++ scripts/install_gitleaks.sh
@@ -6,3 +6,4 @@ VERSION="8.18.2"
 curl -fsSL "https://github.com/gitleaks/gitleaks/releases/download/v${VERSION}/gitleaks_${VERSION}_linux_x64.tar.gz" -o gitleaks.tar.gz
 tar -C /usr/local/bin -xzf gitleaks.tar.gz
 chmod +x /usr/local/bin/gitleaks
+gitleaks version
