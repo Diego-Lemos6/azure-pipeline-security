c8fbd78c741307b4dc7d003cb45bf87570d68222
Diego-Lemos6 <71842566+Diego-Lemos6@users.noreply.github.com>
2024-08-02 12:02:06 -0300

    Update install_trivy_win.ps1

--- scripts/install_trivy_win.ps1
+++ scripts/install_trivy_win.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 
 $VERSION = "0.52.1"
 Invoke-WebRequest -Uri "https://github.com/aquasecurity/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Windows-64bit.zip" -OutFile "trivy.zip"
-Expand-Archive -Path "trivy.zip" -DestinationPath "${env:ProgramFiles}"
+Expand-Archive -Path "trivy.zip" -DestinationPath "${env:ProgramFiles}" -Force
 #Rename-Item -Path "${env:ProgramFiles}\trivy_${VERSION}_Windows-64bit" -NewName "${env:ProgramFiles}\trivy"
